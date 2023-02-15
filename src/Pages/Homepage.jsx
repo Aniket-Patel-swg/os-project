9114f7e6bb8ce60922fa689e361cd3c42ec60e1c
Aniket Patel <patelaniket1207@gmail.com>
2023-02-15 14:29:06 +0530

    Added basic file structure

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../Css/Homepage.css';
+
+const Homepage = () => {
+    return ( 
+        <>  
+        <div className="home-page">
+            Home Page check text
+        </div>
+        </>
+     );
+}
+ 
+export default Homepage;
---
+++ src/Pages/SchedulingAlgo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SchedulingAlgo = () => {
+    return ( 
+        <>
+            Scheduling Algo check text
+        </>
+     );
+}
+ 
+export default SchedulingAlgo;
