0b35666eb2ba63731523e626ca183abb11c672a0
unknown <vpnanavati@gmail.com>
2023-02-17 10:35:34 +0530

    added Homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage.jsx';
 import SchedulingAlgo from './Pages/SchedulingAlgo.jsx';
+import './Css/Homepage.css'
 
 function App() {
   return (
---
+++ src/Pages/Navbar.jsx
@@ -0,0 +1,16 @@
+import '../Css/Navbar.css';
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <div className="img">
+                <img src="computer.png" alt="logo"></img>
+            </div>
+        <a href="/">Home</a>
+        <a href="/">Algorithms</a>
+        <a href="/">Team</a>
+       </nav>
+ );
+}
+
+ 
+export default Navbar ;
