ce47a330a82df77c6f152662491669df86419e00
dev-chapatwala <chapatwaladev@gmail.com>
2023-03-17 02:00:50 +0530

    Added full working project

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage.jsx';
 import SchedulingAlgo from './Pages/SchedulingAlgo.jsx';
-
-
+import FCFSNew from './Pages/FCFSNew.jsx';
 import './Css/Homepage.css'
 import Peterson from './Pages/Peterson';
-import FCFSDisk from './Pages/FCFSDisk';
+// import FCFSDisk from './Pages/FCFSDisk';
 
 function App() {
   return (
@@ -15,9 +14,9 @@ function App() {
          <Routes>
               <Route path='/' element = {<Homepage />}/>
               <Route path='/SchedulingAlgo' element = {<SchedulingAlgo />}/>
-              <Route path='/FCFSDisk' element = {<FCFSDisk />}/>
+              {/* <Route path='/FCFSDisk' element = {<FCFSDisk />}/> */}
               <Route path='/peterson' element={<Peterson />} />
-
+             < Route path='/fcfs' element={<FCFSNew />} />
          </Routes>
     </div>
   );
